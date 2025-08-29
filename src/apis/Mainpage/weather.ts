65874adbbb090eb95f0969514ae2519d03a71048
hairyung2002 <hairyung2002@naver.com>
2025-08-30 05:38:09 +0900

    🐛 Fix:라우팅 수정

--- src/apis/Mainpage/weather.ts
+++ src/apis/Mainpage/weather.ts
@@ -2,7 +2,7 @@ import type { Weather } from '@/types/weather';
 import axios from 'axios';
 
 export const getWeather = async (): Promise<Weather> => {
-  const { data } = await axios.get(`/api/proxy?path=walk`);
+  const { data } = await axios.get(`${import.meta.env.VITE_SERVER_API_URL}/walk`);
 
   return data;
 };
